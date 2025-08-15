caf4dcacf66a9be426b69235e7b67e2e83949dd8
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-08-15 19:14:39 +0800

    feat(keybinds): init

--- config/lua/atelier/config/keybinds.lua
+++ config/lua/atelier/config/keybinds.lua
@@ -1,5 +1,8 @@
 -- leader key
 vim.g.mapleader = " "
 
+-- neo-tree
+vim.keymap.set("n", "<leader>eq", "<cmd>Neotree action=focus<cr>", {})
+
 -- null-ls
 vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, {})
