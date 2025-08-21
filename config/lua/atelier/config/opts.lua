f9a4ba8dc3f183c072c18b07970e76ec88485e17
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-08-21 21:47:50 +0800

    feat(opts): line numbers

--- config/lua/atelier/config/opts.lua
+++ config/lua/atelier/config/opts.lua
@@ -1,4 +1,9 @@
 -- use ripgrep for :grep
 vim.opt.grepprg = "rg --vimgrep"
 
+-- use all colours (fwiw i use ghostty)
 vim.o.termguicolors = true
+
+-- show line numbers and make current line absolute, other lines relative
+vim.opt.number = true
+vim.opt.relativenumber = true
