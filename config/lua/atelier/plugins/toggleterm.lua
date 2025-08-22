12480b8b1498fc088f1d1a8ab9b13838ee724b42
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-08-22 17:22:09 +0800

    fix(lazygit, toggleterm): update neotree on exit instead

--- config/lua/atelier/plugins/toggleterm.lua
+++ config/lua/atelier/plugins/toggleterm.lua
@@ -6,7 +6,7 @@ return {
 
         function set_terminal_keymaps()
             local opts = { buffer = 0 }
-            vim.keymap.set("t", "<C-w>", [[<C-\><C-n>]], opts)
+            vim.keymap.set("t", "<C-w>", [[<C-\><C-n><Cmd>Neotree<CR>]], opts)
             vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
             vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
             vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
