b37f51df2f878c753e6792012ea3d1232e93a4e1
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-08-23 02:49:26 +0800

    refactor(keybinds): open neotree before telescope

--- config/lua/atelier/config/keybinds.lua
+++ config/lua/atelier/config/keybinds.lua
@@ -11,5 +11,5 @@ vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, {})
 -- see plugins/gitsigns.lua
 
 -- telescope
-vim.keymap.set("n", "<leader>FF", "<cmd>Telescope find_files<cr>", { noremap = true, desc = "telescope find files" })
-vim.keymap.set("n", "<leader>FG", "<cmd>Telescope live_grep<cr>", { noremap = true, desc = "telescope ripgrep search" })
+vim.keymap.set("n", "<leader>FF", "<cmd>Neotree<cr><cmd>Telescope find_files<cr>", { noremap = true, desc = "telescope find files" })
+vim.keymap.set("n", "<leader>FG", "<cmd>Neotree<cr><cmd>Telescope live_grep<cr>", { noremap = true, desc = "telescope ripgrep search" })
