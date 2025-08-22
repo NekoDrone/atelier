c32860b8281ded8b4f49a70880143d07f9dda817
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-08-23 02:18:55 +0800

    refactor(greeter): correct telescope keybind

--- config/lua/atelier/config/greet.lua
+++ config/lua/atelier/config/greet.lua
@@ -19,7 +19,7 @@ dashboard.section.header.val = {
 
 dashboard.section.buttons.val = {
     dashboard.button("SPC eq", "  Open file browser..."),
-    dashboard.button("CTRL f", "  Search directory..."),
+    dashboard.button("SPC FF", "  Search project directory..."),
     dashboard.button("SPC GG", "  Open commits menu..."),
 }
 
