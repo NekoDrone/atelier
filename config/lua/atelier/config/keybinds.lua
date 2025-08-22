0f429d8228594f49492527fb65109b11423efbe8
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-08-22 17:23:25 +0800

    fix(keybinds): i gotta stop skill issuing holy shit

--- config/lua/atelier/config/keybinds.lua
+++ config/lua/atelier/config/keybinds.lua
@@ -2,7 +2,7 @@
 vim.g.mapleader = " "
 
 -- neo-tree
-vim.keymap.set("n", "<leader>eq", "<cmd>Neotree action=focus<cr><cmd>Neotree", {})
+vim.keymap.set("n", "<leader>eq", "<cmd>Neotree action=focus<cr><cmd>Neotree<CR>", {})
 
 -- null-ls
 vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, {})
