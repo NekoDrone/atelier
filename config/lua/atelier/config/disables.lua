b6e5f3e0c8838a564ca2a2efb0aa57b8075be497
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-08-24 03:54:51 +0800

    feat(disables): more

--- config/lua/atelier/config/disables.lua
+++ config/lua/atelier/config/disables.lua
@@ -5,3 +5,11 @@ vim.g.loaded_netrwPlugin = 1
 -- more disables here
 -- disable showing mode as we have lualine
 vim.opt.showmode = false
+
+-- relevant shortmess 
+vim.opt.shortmess:append("W")
+vim.opt.shortmess:append("I")
+vim.opt.shortmess:append("S")
+
+-- disable selection count in cmd gutter
+vim.o.showcmd = false
