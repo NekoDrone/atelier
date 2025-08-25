70d6d0ca3d543ed3e81302b440aea1168be6ed9c
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-08-26 03:08:36 +0800

    feat(treesitter): add a crapload of grammars

--- config/lua/atelier/config/treesitter.lua
+++ config/lua/atelier/config/treesitter.lua
@@ -2,10 +2,62 @@
 -- don't forget to install the grammar in the default.nix file!
 vim.api.nvim_create_autocmd("FileType", {
     pattern = {
+        "html",
+        "css",
+        "scss",
+        "javascript",
+        "typescript",
+        "typescriptreact", -- .tsx
+        "javascriptreact", -- .jsx
+        "json",
+        "svelte",
+        "vue",
+        "astro",
+        "xml",
+        "graphql",
+
         "lua",
         "nix",
+        "rust",
+        "go",
+        "gomod",
+        "gosum",
+        "gotmpl",
+        "gowork",
+        "c",
+        "cpp",
+        "zig",
+        "odin",
+
         "vim",
         "bash",
+        "python",
+        "java",
+        "php",
+        "ruby",
+        "elixir",
+        "erlang",
+        "haskell",
+        "ocaml",
+        "fsharp",
+        "swift",
+        "kotlin",
+        "gleam",
+        "elm",
+        "fish",
+        "powershell", -- because i hate myself
+        "pwsh",
+        "yaml", -- because i hate myself
+        "toml",
+        "ini",
+        "csv", -- because i hate myself
+        "sql",
+        "dockerfile",
+        "nginx",
+        "markdown",
+        "tex", -- latex
+        "templ",
+        "tera",
     },
     callback = function()
         vim.treesitter.start()
