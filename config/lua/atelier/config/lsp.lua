172336df28924b651b4bf1c6caaaa724687f842e
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-08-25 22:41:36 +0800

    feat(lsp): inline diagnostics

--- config/lua/atelier/config/lsp.lua
+++ config/lua/atelier/config/lsp.lua
@@ -1,3 +1,39 @@
+-- diganostic config to use our icons
+local diagnostic_config = {
+    [vim.diagnostic.severity.ERROR] = { sign = "", hl = "DiagnosticSignError" },
+    [vim.diagnostic.severity.WARN] = { sign = "", hl = "DiagnosticSignWarn" },
+    [vim.diagnostic.severity.HINT] = { sign = "", hl = "DiagnosticSignHint" },
+    [vim.diagnostic.severity.INFO] = { sign = "", hl = "DiagnosticSignInfo" },
+}
+
+vim.diagnostic.config({
+    signs = {
+        enabled = true,
+        text = (function()
+            local res = {}
+            for key, value in pairs(diagnostic_config) do
+                res[key] = value.sign
+            end
+            return res
+        end)(),
+        numhl = (function()
+            local res = {}
+            for key, value in pairs(diagnostic_config) do
+                res[key] = value.hl
+            end
+            return res
+        end)(),
+    },
+    virtual_text = {
+        enabled = true,
+        spacing = 8,
+        prefix = function(diagnostic)
+            local config = diagnostic_config[diagnostic.severity]
+            return config and config.sign or "●"
+        end,
+    },
+})
+
 -- enable each lsp here
 vim.lsp.enable("lua_ls")
 vim.lsp.config("lua_ls", {
