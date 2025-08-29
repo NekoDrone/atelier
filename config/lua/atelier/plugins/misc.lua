ba014b2562606dec4f0c3445229fb31020a7a701
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-08-29 19:55:38 +0800

    feat(markdown): basic config (thanks izzy)

--- config/lua/atelier/plugins/misc.lua
+++ config/lua/atelier/plugins/misc.lua
@@ -82,11 +82,39 @@ return {
     },
     {
         "markview.nvim",
-        event = "DeferredUIEnter",
+        lazy = false,
         after = function()
+            local p = require("markview.presets")
+
             require("markview").setup({
+                -- allows us to use hybrid mode
                 preview = {
-                    icon_provider = "mini",
+                    modes = { "n", "i", "no", "c" },
+                    hybrid_modes = { "i" },
+                    callbacks = {
+                        on_enable = function(_, win)
+                            vim.wo[win].conceallevel = 2
+                            vim.wo[win].concealcursor = "nc"
+                        end,
+                    },
+                },
+
+                markdown = {
+                    headings = p.headings.glow,
+                    checkboxes = p.checkboxes.nerd,
+                    horizontal_rules = p.horizontal_rules.thin,
+                    code_blocks = {
+                        pad_amount = 2,
+                    },
+                    list_items = {
+                        marker_minus = { add_padding = false },
+                        marker_plus = { add_padding = false },
+                        marker_star = { add_padding = false },
+                        marker_dot = { add_padding = false },
+                    },
+                    preview = {
+                        icon_provider = "mini",
+                    },
                 },
             })
         end,
